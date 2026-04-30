874d81f7f229f93ab1e88ea08e7b9f7683c6dd13
lbarco248-glitch <lbarco248@gmail.com>
2026-04-30 01:22:12 -0500

    Add new method makePayment()

--- src/main/java/Sandbox/AreaA.java
+++ src/main/java/Sandbox/AreaA.java
@@ -18,6 +18,7 @@ public class AreaA {
 
     public static void main(String[] args) throws IOException {
         compiledData();
+        makePayment();
     }
 
 
@@ -45,19 +46,51 @@ public class AreaA {
         bWriter.write(data);
         bWriter.close();
     }
-  /* public static void makePayment() throws IOException {
-      FileWriter fWriter = new FileWriter("Transactions.txt");
-      BufferedWriter bWriter = new BufferedWriter(fWriter);
-
-  } */
 
 
-  public static void time() {
+
+  public static void time()
+  {
       LocalDate originalDate = LocalDate.now();
       DateTimeFormatter formattedDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
       date = originalDate.format(formattedDate);
       LocalTime originalTime = LocalTime.now();
       DateTimeFormatter formattedTime = DateTimeFormatter.ofPattern("hh:mm:ss a");
       time = originalTime.format(formattedTime);
+  }
+    public static void makePayment() throws IOException {
+      FileWriter fWriter = new FileWriter("Transactions.txt");
+      BufferedWriter bWriter = new BufferedWriter(fWriter);
+      time();
+      System.out.println("Payment amount: (Numerical value only)");
+      float payment = input.nextFloat();
+      System.out.println();
+      System.out.println("What is this payment for?\nA) Shopping\nB) Food & Drink\nC) Bills & Utilities\nD) Entertainment\nE) Groceries\nF) Personal\nG) Gas");
+      char paymentCat = input.next().charAt(0);
+      String category = "";
+        switch (paymentCat) {
+            case 'A', 'a' -> category = "Shopping";
+            case 'B', 'b' -> category = "Food & Drink";
+            case 'C', 'c' -> category = "Bills & Utilities";
+            case 'D', 'd' -> category = "Entertainment";
+            case 'E', 'e' -> category = "Groceries";
+            case 'F', 'f' -> category = "Personal";
+            case 'G', 'g' -> category = "Gas";
+            default -> System.out.println("Invalid selection.");
+        }
+        System.out.println("What method of payment was used?\nA) Cash\nB) Check\nC) Debit/Credit Card\nD) Other");
+        char paymentType = input.next().charAt(0);
+        String Type = "";
+        switch (paymentType) {
+            case 'A', 'a' -> Type = "Cash";
+            case 'B', 'b' -> Type = "Check";
+            case 'C', 'c' -> Type = "Debit/Credit Card";
+            case 'D', 'd' -> Type = "Other";
+            default -> System.out.println("Invalid selection.");
+        }
+        String data;
+        data = String.format("%s | %s | %s | %s |$%.2f\n", date, time, category, Type, payment);
+        bWriter.write(data);
+        bWriter.close();
   }
 }
